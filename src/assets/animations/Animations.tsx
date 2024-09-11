66b4b9a0567559280c7e000da2102ca78f7640f3
Guillermo Lopez <virtualcloverdesign@gmail.com>
2024-09-10 22:46:41 -0500

    minor fixes pt6

--- src/assets/animations/Animations.tsx
+++ src/assets/animations/Animations.tsx
@@ -1,19 +1,37 @@
-import { Box } from '@mui/material';
+import { Box, keyframes, styled } from '@mui/material';
+
 import component from '../illustrations/component.svg';
 import doc from '../illustrations/doc.svg';
 import lightning from '../illustrations/lightning.svg';
 import pencil from '../illustrations/pencil.svg';
 import sparks from '../illustrations/sparks.svg';
 
+let rotationAnimation = keyframes`
+0% {transform: rotate(20deg);}
+50% {transform: rotate(0deg);}
+100% {transform: rotate(20deg);}
+`;
+
+let RotatingImg = styled('img')(({ theme }) => ({
+  animationName: rotationAnimation,
+  animationDuration: '4s',
+  animationIterationCount: 'infinite',
+}));
+
 export const InspectAnim = () => {
   return (
     <Box style={{ position: 'relative', marginBottom: 45 }}>
       <img src={doc} style={{ height: 180 }} />
-      <img
+      <RotatingImg
         src={pencil}
-        style={{ position: 'absolute', left: '-40%', bottom: 0, height: 180 }}
+        style={{
+          position: 'absolute',
+          left: '-40%',
+          bottom: 0,
+          height: 180,
+        }}
       />
-      <img
+      <RotatingImg
         src={sparks}
         style={{ position: 'absolute', right: '-15%', top: '-10%' }}
       />
